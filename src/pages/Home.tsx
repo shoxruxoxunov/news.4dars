9a7452a1edd2419367ec5ae866dc7faaa00c61f0
shoxrux <shoxrux@gmail.com>
2024-03-01 02:22:57 +0500

    push

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,21 +7,36 @@ function Home() {
   const { data } = useFetch(url);
 
   return (
-    <div className="pt-[30px] mb-[30px] container flex flex-col gap-5" >
+    <div className="pt-[30px] mb-[30px] container flex flex-col gap-5">
       {data &&
-        data.map((item) => {
+        data.map((item: object | []) => {
           console.log(item);
           return (
             <div key={item.id}>
-              <Link className="card lg:card-side bg-base-100 shadow-xl w-full" to="/news">
+              <Link
+                className="card lg:card-side bg-base-100 shadow-xl w-full"
+                to="/news"
+              >
                 <figure className="md:mr-[80px]">
-                  <img className="md:w-[600px] md:h-[500px] w-[375px] h-[250px]" src={item.images} alt="Album" />
+                  <img
+                    className="md:w-[600px] md:h-[500px] w-[375px] h-[250px]"
+                    src={item.images}
+                    alt="Album"
+                  />
                 </figure>
                 <div className="flex flex-col items-center pt-4 text-center">
-                  <h2 className="text-center md:font-bold md:text-xl  md:w-[400px] w-[300px]">{item.title}</h2>
-                  <p className="md:font-normal md:text-base text-center w-[300px] md:w-[400px]">{item.description}</p>
-                  <p className="md:font-bold md:text-xl font-medium text-base">{item.author}</p>
-                  <p className="md:font-bold md:text-lg font-medium text-base">{item.postedAt}</p>
+                  <h2 className="text-center md:font-bold md:text-xl  md:w-[400px] w-[300px]">
+                    {item.title}
+                  </h2>
+                  <p className="md:font-normal md:text-base text-center w-[300px] md:w-[400px]">
+                    {item.description}
+                  </p>
+                  <p className="md:font-bold md:text-xl font-medium text-base">
+                    {item.author}
+                  </p>
+                  <p className="md:font-bold md:text-lg font-medium text-base">
+                    {item.postedAt}
+                  </p>
                 </div>
               </Link>
             </div>
